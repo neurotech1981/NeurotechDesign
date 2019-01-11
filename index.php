1ddd2748ff383462496c2950dcaec68c036616e2
Bjørn-are Jakobsen <26271316+neurotech1981@users.noreply.github.com>
2019-01-11 21:37:44 +0100

    Update index.php

--- index.php
+++ index.php
@@ -9,128 +9,133 @@
 <meta charset="utf-8">
  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
  <meta name="theme-color" content="#e7ecea"/>
+ <meta name="Description" content="Webdesign og utvikling for små og mellom store bedrifter.">
 <title>Neurotech Design</title>
-<script defer src="https://www.google.com/recaptcha/api.js?hl=no"></script>
-<script defer src="contact.js"></script>
-<!-- <link href="css/boilerplate.css" rel="stylesheet" type="text/css"> -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 <link rel="shortcut icon" href="../favicon.ico">
+<link rel="stylesheet" type="text/css" href="css/fontawesome-all.min.css" />
+<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css" />
+<link rel="stylesheet" type="text/css" href="css/style_footer.css" />
 <link rel="stylesheet" type="text/css" href="css/main.css" />
 <link rel="stylesheet" type="text/css" href="css/tabs.css" />
 <link rel="stylesheet" type="text/css" href="css/tabstyles.css" />
-<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Nunito:400,500,700">
-<!-- Add icon library -->
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+<link rel="stylesheet" type="text/css" href="css/glyphicon.css" />
+<link rel="stylesheet" type="text/css" href="css/animation.css" />
+<link rel="stylesheet" href="//fonts.googleapis.com/css?family=Nunito:300,400,500,600,700" />
+<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" />
+
 </head>
-<body>	
-        <svg class="hidden">
-            <defs>
-                <path id="tabshape" d="M80,60C34,53.5,64.417,0,0,0v60H80z"/>
-            </defs>
-        </svg>
-        
-         <header class="codrops-header">
-<!--<div class="wrap">
-    <div class="outerdiv">
-    <div id="canvas" class="div1">
-
-    </div>    
-    <div class="div2">
-        <br><h1> NEUROTECH DESIGN <span>Webutvikling for små / mellomstore bedrifter og privatpersoner</span></h1>
-    </div>
-    </div>
-</div>-->
-
-<div id="logo"><img src="images/logo_01.png" style="max-width:100%;" alt="" /></div>
-            </header>
+<body>
+  <div class="wrapper">
+  <header class="codrops-header">
+                <div class="logo">
+                  <img src="images/anim_logo_optimized.svg" class="image_full" />
+                  <img src="images/logo_only.svg" class="image_mobile">
+                </div>
+            </header><!--<div id="particles-js"></div>-->
+            <div class="arrow animated bounce"></div>
             <section>
                 <div class="tabs tabs-style-bar">
+                  <div class="content-wrap">
                     <nav>
                         <ul>
-                            <li><a href="#section-bar-1" class="icon icon-home3"><span>Fremside</span></a></li>
-                            <li><a href="#section-bar-2" class="icon icon-users"><span>Klienter</span></a></li>
-                            <li><a href="#section-bar-3" class="icon icon-wrench"><span>Tjenester</span></a></li>
-                            <li><a href="#section-bar-4" class="icon icon-info2"><span>Om oss</span></a></li>
-                            <li><a href="#section-bar-5" class="icon icon-mail"><span>Kontakt oss</span></a></li>
+                            <li><a href="#section-bar-1" class="icon" style="font-size:2.0em;"><i class="fa fa-home"></i>&nbsp;<span>Fremside</span></a></li>
+                            <li><a href="#section-bar-2" class="icon" style="font-size:2.0em;"><i class="fa fa-users"></i>&nbsp;<span>Klienter</span></a></li>
+                            <li><a href="#section-bar-3" class="icon" style="font-size:2.0em;"><i class="fa fa-wrench"></i>&nbsp;<span>Tjenester</span></a></li>
+                            <li><a href="#section-bar-4" class="icon" style="font-size:2.0em;"><i class="fa fa-info-circle"></i>&nbsp;<span>Om oss</span></a></li>
+                            <li><a href="#section-bar-5" class="icon" style="font-size:2.0em;"><i class="fa fa-envelope"></i>&nbsp;<span>Kontakt oss</span></a></li>
                         </ul>
-                    </nav>
-                    <div class="content-wrap">
+                    </nav> 
                         <section id="section-bar-1">
-	                        <div class="jumbotron_fremside"><div class="row">
-		                   <div class="column"> <img alt="Picture of a mac" class="Image" src="/images/front_1_macbook.png" /></div><div class="column">
-                            <h1>Aenean eu leo quam</h1>
-		                        <p class="text_fremside">
-                           Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Sed posuere consectetur est at lobortis. Donec id elit non mi porta gravida at eget metus.<br><br>
-Maecenas faucibus mollis interdum. Nulla vitae elit libero, a pharetra augue. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p><br><br>
-		                   </div></div></div>
+                  <div class="row">
+                    <div class="jumbotron-main" style="margin-bottom: 20px;">
+                        <div class="column">
+                           <img alt="" class="Image" src="/images/tjenester2.svg"  style="margin-top: 40px;" />
+                           <h1>Tjenester</h1>
+                           <hr class="stil1"><p id="text-fremside">Vi utvikler moderne og solide nettside løsninger, i samarbeid med dere så utvikler vi en nettside etter deres forretningsmål.</p>
+                       </div>
+                          <div class="column">
+                             <img alt="" class="Image" src="/images/responsivt_design.svg" style="margin-top: 40px;" />
+                            <h1>Responsivt Design</h1><hr class="stil1">
+                            <p id="text-fremside">Tilpasset alle digitale enheter - med en responsiv nettside vil den tilpasses alle enheter.
+Det er en komplisert prosess å få nettsiden responsiv på alle enheter, og vi legger mye vekt på dette.
+ Vi går for “Mobile first” prinsipp, der nettsiden bygges opp fra bunn med mobile enheter som hovedfokus.
+ </p>
+                       </div>
+                     </div>
+                   </div>
                         </section>
                         <section id="section-bar-2"><p>2</p></section>
                         <section id="section-bar-3"><p>3</p></section>
-                        <section id="section-bar-4"><p>4</p></section>                        
-                        <section id="section-bar-5">      
-                                    <div id="inner">
-                                    <div class="jumbotron_kontakt"> 
+                        <section id="section-bar-4"><p>4</p></section>
+                        <section id="section-bar-5">
+                            <div class="row">
+                                    <div class="jumbotron-kontakt">
                                     <form method="post" id="msform" class="form-inline" enctype="multipart/form-data">
                                     <fieldset>
                                     <!-- Form Name -->
-                                  <div class="column"><h1>Kontakt Info</h1>
-									                   <p>Send meg en <u>melding</u> via kontaktskjemaet, eller <u>ring</u> meg, så kan vi ta en prat om hva du trenger hjelp med.</p><br>
-                                     <div class="kontakt_kort"><br><br>
-                                      <p class="text_kontaktinfo" style="color: #999933;"><i class="glyphicon glyphicon-user">&nbsp;</i>Bjørn-Are&nbsp;Jakobsen</p>
-                                      <p class="text_kontaktinfo" style="color: #999933;">&nbsp;<i class="glyphicon glyphicon-phone">&nbsp;</i>Telefon:&nbsp; <u><a href="tel:47398922">47 39 89 22</a></u></p>
-                                      <p class="text_kontaktinfo" style="color: #999933;">&nbsp;<i class="glyphicon glyphicon-pencil">&nbsp;</i>Org.Nr:&nbsp; <u>920 500 439</u></p><br><br>
-                                    </div>
+                                  <div class="column"><h1>Kontakt Info</h1><hr class="stil1">
+									                   <p id="text-fremside">Send meg en <u>melding</u> via kontaktskjemaet, eller <u>ring</u> meg, så kan vi ta en prat om hva du trenger hjelp med.</p><br>
+                                      <div class="kontakt-kort"><br><br>
+                                        <p id="text-kontaktinfo" style="color: #003333;">&nbsp;<i class="glyphicon glyphicon-user" style="font-size: 1.2em;">&nbsp;</i>Bjørn-Are&nbsp;Jakobsen</p><hr class="stil1">
+                                        <p id="text-kontaktinfo" style="color: #003333;">&nbsp;<i class="glyphicon glyphicon-phone" style="font-size: 1.2em;">&nbsp;</i>Telefon:&nbsp; <a href="tel:47398922">47 39 89 22</a></p><hr class="stil1">
+                                        <p id="text-kontaktinfo" style="color: #003333;">&nbsp;<i class="glyphicon glyphicon-envelope" style="font-size: 1.2em;">&nbsp;</i><span>E-post:</span>&nbsp; <u><a href="mailto:ba.jakobsen@gmail.com">ba.jakobsen@gmail.com</a></p><br><br>
+                                      </div>
                                   </div>
                                     <!-- Text input-->
-                                    <div class="column"><h1 style="margin-bottom: 10px;">Kontaktskjema</h1>
-                                  <div class="control-group">
-                                      <label class="control-label" for="txtEpost"></label>
+                                <div class="column">
+                                    <h1 style="margin-bottom: 10px;">Kontaktskjema</h1><hr class="stil1">
+                                      <div class="control-group">
+                                        <label class="control-label" for="txtEpost"></label>
                                       <div class="controls"><span style="text-align: left"></span>
-                                      <div class="left-inner-addon">
-                                        <i class="glyphicon glyphicon-user"></i>
-                                       <!-- <input type="text" class="form-control" />-->
-                                         <input name="sender_name" type="text" required class="input-medium" 
-                                         id="sender_name" placeholder="Ditt navn" autocomplete="on" size="30" >
-                                            </div>
+                                        <div class="left-inner-addon">
+                                                <i class="glyphicon glyphicon-user"></i>
+                                                <!-- <input type="text" class="form-control" />-->
+                                                 <input name="sender_name" type="text" required class="input-medium"
+                                                 id="sender_name" placeholder="Ditt navn *" autocomplete="on" size="30" >
                                         </div>
+                                      </div>
                                     </div>
                                     <!-- Text input-->
                                     <div class="control-group">
                                       <label class="control-label" for="txtEpost"></label>
-                                      <div class="controls">
-                                        <div class="left-inner-addon">
-                                        <i class="glyphicon glyphicon-envelope"></i>
-                                        <input name="sender_email" type="email" required class="input-medium" 
-                                        id="sender_email" placeholder="E-Post" autocomplete="on" size="30">
+                                        <div class="controls">
+                                          <div class="left-inner-addon">
+                                            <i class="glyphicon glyphicon-envelope"></i>
+                                            <input name="sender_email" type="email" required class="input-medium"
+                                            id="sender_email" placeholder="E-Post *" autocomplete="on" size="30">
                                         </div>
                                       </div>
                                     </div>
                                     <!-- Textarea -->
                                     <div class="control-group">
                                       <label class="control-label" for="txtMelding"></label>
-                                      <div class="left-inner-addon">                     
-                                        <textarea name="sender_message" placeholder="Din melding..." cols="38" rows="7" id="sender_message" required class="input-medium"></textarea>
+                                        <div class="left-inner-addon">
+                                          <textarea name="sender_message" placeholder="Din melding..." cols="38" rows="7" id="sender_message" required class="input-medium"></textarea>
                                       </div>
                                     </div>
                                     <!-- Button -->
                                     <div class="control-group">
                                       <label class="control-label" for="btnSend"></label>
-                                      <div class="left-inner-addon"><div class="g-recaptcha" style="display: inline-block; padding: 2px;" data-sitekey="6Lc_TUoUAAAAANWp9eWEsszFOxon-EEM8wtsvxsU"></div>
-                                        <button id="btnSend" name="Submit" type="submit" value="Submit" class="btn-kontakt">
-                                         Send&nbsp;<span class="glyphicon glyphicon-send"></span></button>
-                                         <br>
+                                        <div class="left-inner-addon"><div class="g-recaptcha" style="display: inline-block; padding: 2px;" data-sitekey="6Lc_TUoUAAAAANWp9eWEsszFOxon-EEM8wtsvxsU"></div>
+                                          <button id="btnSend" name="Submit" type="submit" value="Submit" class="btn-kontakt">
+                                          Send melding&nbsp;&nbsp;<span style="width: auto;" class="far fa-paper-plane"></span></button>
+                                          <br>
                                           <div id="error_message" class="ajax_response"></div>
                                           <div id="success_message" class="ajax_response"></div>
                                           <br>
+                                      </div></div></div></div>
+                                    </div></div></div></section>
+                                    <footer class="footer">
+                                      <div class="content">
+                                      <div class="copyright">© Neurotech-Design 2018 <br>Org.nr. 920 500 439</div>
+                                      <div class="social">
+                                        <a href="https://facebook.com/ondrej.p.barta" class="link facebook" target="_parent"><span class="fab fa-facebook-square"></span></a>
+                                        <a href="https://twitter.com/PageOnlineXS" class="link twitter" target="_parent"><span class="fab fa-twitter"></span></a>
+                                        <a href="https://plus.google.com/+OndřejBárta-Otaku" class="link instagram" target="_parent"><span class="fab fa-instagram"></span></a>
                                       </div>
-                                    </div>
-                                    </div></div>
-                                    </form></div></div></div>
-                                    </div></section>
-                                    <script src="js/jquery-3.3.1.min.js"></script>
-                                    <script defer src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-                                 <script src="js/cbpFWTabs.js"></script>
+                                      </div>
+                                    </footer>
+                              <script src="js/cbpFWTabs.js"></script>
                                   <script>
                                       (function() {
                                           [].slice.call( document.querySelectorAll( '.tabs' ) ).forEach( function( el ) {
@@ -138,11 +143,20 @@ Maecenas faucibus mollis interdum. Nulla vitae elit libero, a pharetra augue. Nu
                                           });
                                       })();
                                   </script>
-                                  </body>
-                                    <!-- /tabs -->
-                             <footer class="footer"><p>
-                        <div class="social-btns"><a class="btn facebook" href="#"><i class="fa fa-facebook"></i></a><a class="btn twitter" href="#"><i class="fa fa-twitter"></i></a>
-                </div></p>
-                      		</footer>
-                </html>
-
+                                  <!--<script src="js/particles.min.js"></script>-->
+                                  <script defer src="js/jquery-3.3.1.min.js"></script>
+                                  <script defer src="//www.google.com/recaptcha/api.js?hl=no"></script>
+                                  <script defer src="contact.js"></script>
+                                  <script defer src="js/fontawesome-all.min.js"></script>
+                                  <script defer src="js/app.js"></script>
+                                  <script defer src="js/footer_fixed.js"></script>
+                                  <script src='https://cdnjs.cloudflare.com/ajax/libs/prefixfree/1.0.7/prefixfree.min.js'></script>
+                                  <script>
+                                    if('serviceWorker' in navigator) {
+                                      navigator.serviceWorker
+                                              .register('app/sw.js')
+                                              .then(function() { console.log("Service Worker Registered"); });
+                                    }
+                                  </script>
+      </body>
+</html>
